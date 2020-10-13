ca05f4fd0e7347fd3de3d16a66b2110491db7afd
Natee159 <s6004062630248@email.kmutnb.ac.th>
2020-10-13 21:03:48 +0700

    edit photo

--- src/page/insert.js
+++ src/page/insert.js
@@ -4,11 +4,11 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import { useHistory } from "react-router-dom";
 import getCookie from '../component/getCookie.js';
 const Insert = () => {
+      const [selectedFile, setSelectedFile] = useState(null)
       const [Product_name, setProduct_name] = useState()
       const [Author_name, setAuthor_name] = useState()
       const [Publi_name, setPubli_name] = useState()
       const [Detail, setDetail] = useState()
-      const [Image, setImage] = useState()
       const [Total, setTotal] = useState()
       const [Price, setPrice] = useState()
       const [Category_ID, setCategory_ID] = useState()
@@ -17,12 +17,15 @@ const Insert = () => {
       const handleSubmit = event => {
             event.preventDefault();
             alert('insert');
+            const fd = new FormData();
+            fd.append('image', selectedFile, selectedFile.name);
+            axios.post(`http://localhost/api/product/uploadimg.php`, fd)
             axios.post(`http://localhost/api/product/create.php`, JSON.stringify({
                   "Product_name": Product_name,
                   "Author_name": Author_name,
                   "Publi_name": Publi_name,
                   "Detail": Detail,
-                  "Image": Image,
+                  "Image": selectedFile.name,
                   "Total": Total,
                   "Price": Price,
                   "Category_ID": Category_ID,
@@ -32,13 +35,15 @@ const Insert = () => {
                         console.log(res);
                         console.log(res.data);
                   })
+            history.push("/admin")
+
       }
 
       useEffect(() => {
-                if(getCookie("username") === ""){
-                    history.push("/")
-                  }
-        }, []);
+            if (getCookie("username") === "") {
+                  history.push("/")
+            }
+      }, []);
 
       return (
             <Container>
@@ -83,7 +88,7 @@ const Insert = () => {
                                                 <p>Image</p>
                                           </Col>
                                           <Col>
-                                                <input type="text" onChange={e => { setImage(e.target.value) }} />
+                                                <input type="file" onChange={e => { setSelectedFile(e.target.files[0]) }} />
                                           </Col>
                                     </Row>
                                     <Row>
